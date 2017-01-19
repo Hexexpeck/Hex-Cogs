dd7dd55b4af6059e82c25350ab30d2b78525917e
Hexexpeck <Hexexpeck@users.noreply.github.com>
2017-01-18 22:13:49 -0500

    [Dank] Fix bug where bot would say a rating for itself
    It should now just say that the bot is the best and shouldn't rate the bot.

--- dank/dank.py
+++ dank/dank.py
@@ -20,23 +20,22 @@ class Dank:
         if user.id == self.bot.user.id:
             await self.bot.say("m8 dont even try to r8 the donkest person in this universe m8. im too donk for you to handle.")
         else:
-            pass
-        donkrating = {}
-        donkrating['1'] = "{0}, your donk-o-meter rating is 1/10. ew get away from me".format(person)
-        donkrating['2'] = "{0}, your donk-o-meter rating is 2/10. gtfo".format(person)
-        donkrating['3'] = "{0}, your donk-o-meter rating is 3/10. bad".format(person)
-        donkrating['4'] = "{0}, your donk-o-meter rating is 4/10. sorta bad".format(person)
-        donkrating['5'] = "{0}, your donk-o-meter rating is 5/10. not bad and not good.".format(person)
-        donkrating['6'] = "{0}, your donk-o-meter rating is 6/10. sorta improving.".format(person)
-        donkrating['7'] = "{0}, your donk-o-meter rating is 7/10. ok things are starting to get donk.".format(person)
-        donkrating['8'] = "{0}, your donk-o-meter rating is 8/10. well then someone is donk.".format(person)
-        donkrating['9'] = "{0}, your donk-o-meter rating is 9/10. ( ͡° ͜ʖ ͡°)".format(person)
-        donkrating['10'] = "{0}, your donk-o-meter rating is 10/10. ayy lmao you dank af m9.".format(person)
-        donkrating['11'] = "{0}, your donk-o-meter rating is 11/10. m8 how tho only the donkest of the donk get this rating.".format(person)
-        donkrating['broken'] = "{0}, your donk-o-meter rating was so donk it broke the scale m8.".format(person)
-        donkrating['unknown'] = "{0}, your donk-o-meter rating is so low you can't even get 0 on the scale.".format(person)
+            donkrating = {}
+            donkrating['1'] = "{0}, your donk-o-meter rating is 1/10. ew get away from me".format(person)
+            donkrating['2'] = "{0}, your donk-o-meter rating is 2/10. gtfo".format(person)
+            donkrating['3'] = "{0}, your donk-o-meter rating is 3/10. bad".format(person)
+            donkrating['4'] = "{0}, your donk-o-meter rating is 4/10. sorta bad".format(person)
+            donkrating['5'] = "{0}, your donk-o-meter rating is 5/10. not bad and not good.".format(person)
+            donkrating['6'] = "{0}, your donk-o-meter rating is 6/10. sorta improving.".format(person)
+            donkrating['7'] = "{0}, your donk-o-meter rating is 7/10. ok things are starting to get donk.".format(person)
+            donkrating['8'] = "{0}, your donk-o-meter rating is 8/10. well then someone is donk.".format(person)
+            donkrating['9'] = "{0}, your donk-o-meter rating is 9/10. ( ͡° ͜ʖ ͡°)".format(person)
+            donkrating['10'] = "{0}, your donk-o-meter rating is 10/10. ayy lmao you dank af m9.".format(person)
+            donkrating['11'] = "{0}, your donk-o-meter rating is 11/10. m8 how tho only the donkest of the donk get this rating.".format(person)
+            donkrating['broken'] = "{0}, your donk-o-meter rating was so donk it broke the scale m8.".format(person)
+            donkrating['unknown'] = "{0}, your donk-o-meter rating is so low you can't even get 0 on the scale.".format(person)
 
-        await self.bot.say('**{0}**'.format(random.choice([donkrating[i] for i in donkrating])))
+            await self.bot.say('**{0}**'.format(random.choice([donkrating[i] for i in donkrating])))
 
     async def lmao(self, ctx):
         if message.content.startswith("ayy"):
