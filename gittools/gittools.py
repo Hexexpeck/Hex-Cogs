de50a6fa657e50f54c9acf5f39739bb263714a50
Kowlin <boxedpp@gmail.com>
2017-08-15 22:33:12 +0200

    The commit that unfucks most of this.

--- gittools/gittools.py
+++ gittools/gittools.py
@@ -3,18 +3,22 @@ import sys
 from subprocess import check_output
 import discord
 from discord.ext import commands
+from cogs.utils import checks
+
 
 class GitTools:
 
     def __init__(self, bot):
         self.bot = bot
 
+    @checks.is_owner()
     @commands.group(pass_context=True)
     async def git(self, ctx):
         """Git related commands."""
         if ctx.invoked_subcommand is None:
             await self.bot.say("You do know that you need to pass in a subcommand, right? Use [p]help git for more info.")
 
+    @checks.is_owner()
     @git.command(pass_context=True)
     async def clone(self, ctx, *, repo: str):
         """Lets you git clone a repository. Requires that you have Git installed and a valid repository URL."""
@@ -28,6 +32,7 @@ class GitTools:
             print("[INFO-GitTools]: Successfully git cloned repository.")
             await self.bot.say("Successfully git cloned repository **{}**".format(repo))
 
+    @checks.is_owner()
     @git.command(pass_context=True)
     async def coginfo(self, ctx):
         """Info about this cog."""
